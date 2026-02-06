6491529ab27673b05d2bf819029479fb27ebe57a
Enrico Gargiulo <enrico@192.168.1.65>
2026-02-06 22:54:57 +0100

    v0.6.2: Modal Centered & Swipe Clipping Fix

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 export const CONFIG = {
-    APP_VERSION: '0.6.1',
+    APP_VERSION: '0.6.2',
     APP_NAME: '7th Sea',
     BUILD_DATE: new Date().toLocaleDateString('it-IT')
 };
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = '7thsea-v0.6.1';
+const CACHE_NAME = '7thsea-v0.6.2';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
