a2abb950c9857e0f878d56686d3560cac1f0d2d5
Enrico Gargiulo <enrico@192.168.1.67>
2026-02-09 00:36:22 +0100

    Bump to v0.9.61 (User tweaked dice pos)

--- js/app.js
+++ js/app.js
@@ -2,15 +2,15 @@ import { Router } from './router.js';
 import { CONFIG } from './config.js';
 import CharacterList from './components/CharacterList.js';
 import DiceRoller from './components/DiceRoller.js';
-import Settings from './components/Settings.js?v=0.9.60';
-import CharacterSheet from './components/CharacterSheet.js?v=0.9.60';
+import Settings from './components/Settings.js?v=0.9.61';
+import CharacterSheet from './components/CharacterSheet.js?v=0.9.61';
 import CreateWizard from './components/CreateWizard.js';
 import { AdventureTab } from './components/AdventureTab.js';
-import { AuthService } from './services/AuthService.js?v=0.9.60';
-import { CampaignDetail } from './components/CampaignDetail.js?v=0.9.60';
-import { Dice } from './dice.js?v=0.9.60';
-import { Storage } from './storage.js?v=0.9.60';
-import { CampaignService } from './services/CampaignService.js?v=0.9.60';
+import { AuthService } from './services/AuthService.js?v=0.9.61';
+import { CampaignDetail } from './components/CampaignDetail.js?v=0.9.61';
+import { Dice } from './dice.js?v=0.9.61';
+import { Storage } from './storage.js?v=0.9.61';
+import { CampaignService } from './services/CampaignService.js?v=0.9.61';
 
 class App {
     constructor() {
